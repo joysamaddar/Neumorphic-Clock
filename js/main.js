25b182ef3823b5151f88c318a7331eb144bdef8a
Joy <56669600+joysamaddar@users.noreply.github.com>
2020-08-29 22:11:36 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const ahour = document.querySelector(".inner-circle .hours");
+const amin = document.querySelector(".inner-circle .minutes");
+const asec = document.querySelector(".inner-circle .seconds");
+const digital = document.querySelector(".time");
+
+(function innit() {
+  setInterval(() => {
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    digital.innerHTML = `${hour.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+    })}:${min.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+    })}:${sec.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+    })}`;
+    asec.style.transform = `rotate(${-90 + (360 / 60) * sec}deg)`;
+    amin.style.transform = `rotate(${-90 + (360 / 60) * min}deg)`;
+    ahour.style.transform = `rotate(${-90 + (360 / 12) * hour}deg)`;
+  }, 1000);
+})();
